8edf301ea343e8b24177c5cae9e06d8eb64f2e2c
Chitra Srinivasan <csriniv@ncsu.edu>
2022-09-01 12:19:07 -0400

    created an object for testing, setting up API testing

---
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/api/APITest.java
@@ -0,0 +1,67 @@
+package edu.ncsu.csc.CoffeeMaker.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import edu.ncsu.csc.CoffeeMaker.common.TestUtils;
+import edu.ncsu.csc.CoffeeMaker.models.Recipe;
+
+@RunWith ( SpringRunner.class )
+@SpringBootTest
+@AutoConfigureMockMvc
+public class APITest {
+
+    /**
+     * MockMvc uses Spring's testing framework to handle requests to the REST
+     * API
+     */
+    private MockMvc               mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    /**
+     * Sets up the tests.
+     */
+    @Before
+    public void setup () {
+        mvc = MockMvcBuilders.webAppContextSetup( context ).build();
+    }
+
+    @Test
+    @Transactional
+    public void testAPI () throws Exception {
+        final String recipe = mvc.perform( get( "/api/v1/recipes" ) ).andDo( print() ).andExpect( status().isOk() )
+                .andReturn().getResponse().getContentAsString();
+        if ( !recipe.contains( "Mocha" ) ) {
+            // create a new Mocha recipe
+            final Recipe r = new Recipe();
+            r.setChocolate( 2 );
+            r.setCoffee( 3 );
+            r.setMilk( 4 );
+            r.setName( "Mocha" );
+            r.setPrice( 5 );
+            r.setSugar( 6 );
+
+            mvc.perform( post( "/api/v1/recipes" ).contentType( MediaType.APPLICATION_JSON )
+                    .content( TestUtils.asJsonString( r ) ) ).andExpect( status().isOk() );
+
+        }
+    }
+}
